75dd2cd63b3b29ca36291e8570c3aaa868812dd0
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-12-03 09:41:13 +0000

    Installing gdown directly from Dockerfile

--- app/ingest_data.sh
+++ app/ingest_data.sh
@@ -2,6 +2,6 @@
 # zip test.py.zip test.py
 # unzip test.py.zip -d data/
 
-gdown https://drive.google.com/uc?id=16YKyFkYlvawCHv3W7WuTFzM8RYgMRWMt&export=download
+#gdown https://drive.google.com/uc?id=16YKyFkYlvawCHv3W7WuTFzM8RYgMRWMt&export=download
 #sleep 30
 unzip -o *.zip -d data/
--- app/run.sh
+++ app/run.sh
@@ -1,2 +1,4 @@
-docker compose up --build
-docker exec -t altcoin_spike_clustering sh ingest_data.sh
+#docker compose up --build
+#docker exec -t altcoin_spike_clustering sh ingest_data.sh
+
+sh ingest_data.sh
