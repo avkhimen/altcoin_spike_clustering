83cb19fa7ea9170ab04163ec4c6e07c05d030ad4
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-12-06 02:57:35 +0000

    Adding check for presence of data

--- app/get_data.py
+++ app/get_data.py
@@ -1,3 +1,13 @@
 import gdown
+import os 
+  
+# path of the directory 
+path = os.getcwd() + "/data"
+  
+# Getting the list of directories 
+dir = os.listdir(path) 
+  
+# Checking if the list is empty or not 
+if len(dir) == 0: 
+    gdown.download("https://drive.google.com/uc?id=16YKyFkYlvawCHv3W7WuTFzM8RYgMRWMt&export=download")
 
-gdown.download("https://drive.google.com/uc?id=16YKyFkYlvawCHv3W7WuTFzM8RYgMRWMt&export=download")
