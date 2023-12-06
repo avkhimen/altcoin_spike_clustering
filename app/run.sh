b05b71360d168d57e978d86f0f525e4075596329
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-12-06 06:54:05 +0000

    Adjusting run.sh to remove unnesarry files

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,20 @@
 python3 get_data.py
 unzip -o *.zip -d data/
 rm *.zip
+cd data/
+sudo rm *_1.csv
+sudo rm *_1440.csv
+sudo rm *_15.csv
+sudo rm *_720.csv
+sudo rm *_5.csv
+sudo rm *USD*
+sudo rm *CAD*
+sudo rm *AUD*
+sudo rm *EUR*
+sudo rm *JPY*
+sudo rm *GBP*
+sudo rm *CHF*
+sudo rm *AED*
+sudo rm *ETH_60.csv
+sudo rm *DAI_60.csv
+sudo rm *DOT_60.csv
